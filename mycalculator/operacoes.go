5633d05720218292661469d6c907b18cba0e64fd
Kaiohenriqueps <kh.silva@hotmail.com>
2021-03-05 14:02:06 -0300

    adding comments in funcs and var

--- mycalculator/operacoes.go
+++ mycalculator/operacoes.go
@@ -1,5 +1,6 @@
 package mycalculator
 
+// Add é uma função que soma dois números.
 func Add(x int, y int) int {
 	return x + y
 }
--- mycalculator/operacoesX.go
+++ mycalculator/operacoesX.go
@@ -1,7 +1,9 @@
 package mycalculator
 
+// A é uma string.
 var A string = "KAIOOOO"
 
+// AddX é uma função que soma 10 com o valor passado por parâmetro.
 func AddX(a int) int {
 	return a + 10
 }
